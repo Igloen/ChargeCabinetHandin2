19801b2bf3ef83a4fe909ffc9810a2b54533b092
Kristian <kristianjohan@outlook.dk>
2020-03-11 14:45:08 +0100

    ChargeControl added

---
+++ ChargeCabinetLibrary/ChargeControl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeCabinetLibrary
+{
+    public class ChargeControl
+    {
+        public bool IsConnected()
+        {
+            return true;
+        }
+
+        public void StartCharge()
+        {
+
+        }
+
+        public void StopCharge()
+        {
+
+        }
+
+
+    }
+}
---
+++ ChargeCabinetLibrary/RFidReader.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChargeCabinetLibrary
+{
+    public class RFidReader
+    {
+        private int id;
+
+        public event EventHandler<RFidChangedEventArgs> RFidChangedEvent;
+
+        public void SetID(int newID)
+        {
+            OnRFidChanged(new RFidChangedEventArgs { ID = newID});
+        }
+
+
+        protected virtual void OnRFidChanged(RFidChangedEventArgs e)
+        {
+            RFidChangedEvent?.Invoke(this, e);
+        }
+
+    }
+
+
+
+    public class RFidChangedEventArgs : EventArgs
+    {
+        public int ID { get; set; }
+    }
+}
+
