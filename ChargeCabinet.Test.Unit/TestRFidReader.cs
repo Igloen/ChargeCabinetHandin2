ded65b361c98e37921c0854eaf02d81d6149e636
Martin Bojesen <Au616699@uni.au.dk>
2020-03-19 14:16:28 +0100

    Test af RFid

---
+++ ChargeCabinet.Test.Unit/TestRFidReader.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using ChargeCabinetLibrary;
+using NSubstitute;
+
+namespace ChargeCabinet.Test.Unit
+{
+    [TestFixture]
+    public class TestRFidReader
+    {
+        private IRFidReader _uut;
+        private RFidChangedEventArgs _receivedEventArgs;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+
+            _uut = new RFidReader();
+
+            _receivedEventArgs = null;
+
+            _uut.RFidChangedEvent += (o, args) =>
+            {
+                _receivedEventArgs = args;
+
+            };
+        }
+
+        [TestCase(123)]
+        [TestCase(321)]
+        public void RFidEventArgsIsNotNull(int id)
+        {
+            _uut.SetID(id);
+            Assert.That(_receivedEventArgs, Is.Not.Null);
+        }
+
+
+        [TestCase(345)]
+        public void SetID_Test(int id)
+        {
+            _uut.SetID(id);
+            Assert.That(_receivedEventArgs.ID, Is.EqualTo(id));
+        }
+
+    }
+}
